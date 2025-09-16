ed11e5ba3e1ae9e76e9efe024cc590b0dd188221
Jazza231 <greeny-231@outlook.com>
2025-09-16 15:51:36 +0800

    Simple gif making command

---
+++ ffmpeg-gif.ps1
@@ -0,0 +1,4 @@
+# ffmpeg -y -framerate [FPS] -start_number 1 -i "[input image with %d as number]" -gifflags +offsetting+transdiff -filter_complex "split[s0][s1];[s0]palettegen=stats_mode=single:max_colors=64[p];[s1][p]paletteuse=dither=bayer:bayer_scale=3:diff_mode=rectangle" -loop 0 "[output.gif]"
+
+ffmpeg -y -framerate 10 -start_number 1 -i "C:\Users\jazza\Downloads\wplace\data\%d-mode.png" -gifflags +offsetting+transdiff -filter_complex "split[s0][s1];[s0]palettegen=stats_mode=single:max_colors=64[p];[s1][p]paletteuse=dither=bayer:bayer_scale=3:diff_mode=rectangle" -loop 0 "C:\Users\jazza\Downloads\wplace\animated.gif"
+

378ccc4de4a7542d4fdf9d4afcf05798998acdf5
Jazza231 <greeny-231@outlook.com>
2025-09-27 18:11:15 +0800

    Up a bit more?

--- config.ts
+++ config.ts
@@ -47,7 +47,7 @@ export const DEFAULT_CONFIG = {
 	/** Number of splits to use; WP_SPLITS env var */
 	SPLITS: 8,
 	/** Number of concurrent tasks to run; WP_CONCURRENT env var */
-	CONCURRENT: 1500,
+	CONCURRENT: 1800,
 	/** INCLUSIVE, the bounds of tiles to process; no env var, isn't configurable */
 	TILE_BOUNDS: {
 		minX: 0,
